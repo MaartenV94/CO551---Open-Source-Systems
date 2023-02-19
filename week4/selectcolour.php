62545dd9cf2be418dac5f7603210451d9004687f
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2023-02-19 14:10:18 +0000

    wk4ex1 Completed

--- week4/selectcolour.php
+++ week4/selectcolour.php
@@ -3,14 +3,15 @@
     <body>
       <form action="confirmation.php"  method="post">
 	Select the colour for the <?php echo $_POST["selqty"] ?> widgets you are ordering
-   	<select name="selcolour">
-   	  <option>white</option>
-	  <option>red</option>
-	  <option>yellow</option>
-	  <option>green</option>
-	  <option>blue</option>
+   	    <select name="selcolour">
+   	      <option>white</option>
+	        <option>red</option>
+	        <option>yellow</option>
+	        <option>green</option>
+	        <option>blue</option>
         </select>
         <br/><br/>	
+        <input type="hidden" name="hdqty" value ="<?php echo $_POST["selqty"] ?>" />
         <input type="submit" value="Buy"/>
       </form>
    </body>
