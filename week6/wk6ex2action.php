6a52452ea05d478ed7a1d610ef0cb9831273ff12
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2023-03-07 11:39:39 +0000

    wk6ex2 Completed

--- week6/wk6ex2action.php
+++ week6/wk6ex2action.php
@@ -1,9 +1,10 @@
 <?php	
 
 	// Connect to server and select database
-    $conn - new sqli('localhost', 'root','','db1_gwalke01')
+    $conn = new mysqli("localhost","root","","db1_gwalke01");
 
-	$sql = "SELECT * from test where name = '$_GET[id]'";
+	$sql = "SELECT * FROM test WHERE ID = '$_GET[id]' ";
+
 	// Execute query
     $result = $conn->query($sql);
 
@@ -12,17 +13,20 @@
 
 <html>
 <body>
-<form action="wk6ex2.php" method="POST">
+<form action="" method="post">
 
 	Name :
-	<input type=text name=txtname value="<? echo $row[name] ?>" readonly />
+	<input type=text name=txtname value="<?php echo $row["name"] ?>" readonly />
 	</br>
 	Phone number :
-	<input type=text name=txttelno value="<? echo $row[phone_number] ?>" />
+	<input type=text name=txttelno value="<?php echo $row["phone_number"]?>" />
 	</br>
 	Email :
-	<input type=text name=txtemail value="<? echo $row[email] ?>" />
+	<input type=text name=txtemail value="<?php echo $row["email"]?>" />
 	</br>
+	
 	<input type=submit name=btnsubmit value="save"/>
+
 </form>
 </body>
+
