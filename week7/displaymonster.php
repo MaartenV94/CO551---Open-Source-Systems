7a2da116ffe5db0676fd5ff50b0e4a54cc3b1aee
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2023-03-21 15:27:26 +0000

    wk7 ex2 & 3 completed

---
+++ week7/displaymonster.php
@@ -0,0 +1,13 @@
+<?php
+    
+    $conn = mysqli_connect("localhost","21424839","mysqluser","db5_21424839");
+
+$sql = "select id from monster;";
+
+$result = mysqli_query($sql, $db);
+$row = mysqli_fetch_array($result);
+
+echo "<img src='getjpg.php?id=" . $row[id]. "'/>";
+
+mysqli_close();
+?>
