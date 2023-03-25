87076aafd32bdf7c744c8d0099fdbf5487a891e0
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2023-03-25 12:30:40 +0000

    Update 3

--- week7/displaymonster.php
+++ week7/displaymonster.php
@@ -4,7 +4,7 @@ $conn = mysqli_connect("localhost", "21424839", "mysqluser", "db5_21424839");
 
 $sql = "select id from monster;";
 
-$result = mysqli_query($sql, $db);
+$result = mysqli_query($sql, $conn);
 $row = mysqli_fetch_array($result);
 
 echo "<img src='getjpg.php?id=" . $row[id]. "'/>";
