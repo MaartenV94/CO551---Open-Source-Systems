80e2a89a4fdfacb18cf1fde598f33efd3303a1d0
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2023-03-26 11:55:16 +0100

    wk8ex3 update 6

--- week8/wk8ex3.php
+++ week8/wk8ex3.php
@@ -8,7 +8,10 @@
         }
         $conn = mysqli_connect("localhost","21424839","mysqluser","db5_21424839");
 
-        $sql = "INSET INTO lotto (lottodate,number1,number2,number3,number4,number5,number6) VALUES ('$lottodate','$number[1]','$number[2]','$number[3]','$number[4]','$number[5]','$number[6]');";
+        $sql = "insert into lotto (lottodate,number1,number2,";
+        $sql = $sql . "number3,number4,number5,number6)";
+        $sql = $sql . " values ($lottodate,$number[1],$number[2],";
+        $sql = $sql . "$number[3],$number[4],$number[5],$number[6]);";
 
         mysqli_query($conn, $sql);
         echo "<br/>This weeks numbers have been saved";
